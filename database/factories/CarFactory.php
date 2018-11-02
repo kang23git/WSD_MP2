dea4332090145cfcadad63cb2626788fce798c2d
shreya <sk2645@njit.edu>
2018-11-02 12:48:22 -0400

    Task:Created Car Seeder and generated 50 cars.

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Car::class, function (Faker $faker) {
     return [
         'make' => $faker->randomElement(['ford','honda','toyota']),
-        'model' => $faker->numberBetween($min = 1000, $max = 9000),
+        'model' => $faker->randomElement(['XN','UV','AM','QR']),
         'year' => $faker->year($max = 'now'),
     ];
 });
